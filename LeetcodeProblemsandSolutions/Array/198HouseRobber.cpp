bd998ba6b6fc24c75f1b69c125e27de24202a385
DevMayurJ <jadhav.mayur30@gmail.com>
2022-02-18 13:59:36 +0530

    Added 198HouseRobber.cpp

---
+++ LeetcodeProblemsandSolutions/Array/198HouseRobber.cpp
@@ -0,0 +1,93 @@
+/*
+You are a professional robber planning to rob houses along a street. Each house has a certain amount of money stashed, the only constraint stopping you from robbing each of them is that adjacent houses have security systems connected and it will automatically contact the police if two adjacent houses were broken into on the same night.
+
+Given an integer array nums representing the amount of money of each house, return the maximum amount of money you can rob tonight without alerting the police.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3,1]
+Output: 4
+Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+Total amount you can rob = 1 + 3 = 4.
+Example 2:
+
+Input: nums = [2,7,9,3,1]
+Output: 12
+Explanation: Rob house 1 (money = 2), rob house 3 (money = 9) and rob house 5 (money = 1).
+Total amount you can rob = 2 + 9 + 1 = 12.
+
+//https://www.geeksforgeeks.org/maximum-sum-such-that-no-two-elements-are-adjacent/
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+	int rob(vector<int>& nums)
+	{
+		if (nums.empty())
+		{
+			return 0;
+		}
+
+		int iESum = 0;
+		int iISum = nums[0];
+		for (size_t i = 1; i < nums.size(); ++i)
+		{
+			int iNewSum = iISum;
+			if (iESum > iISum)
+			{
+				iNewSum = iESum;
+			}
+
+			iISum = iESum + nums[i];
+			iESum = iNewSum;
+		}
+
+		return (iESum > iISum) ? iESum : iISum;
+	}
+};
+
+void printArray(vector<int> vctArr)
+{
+	for (auto it : vctArr)
+	{
+		cout << it << ", ";
+	}
+}
+
+int main()
+{
+	vector<vector<int>> vctInput = {
+						{1,2,3,1},
+						{2,7,9,3,1},
+	};
+
+	int iRet;
+	Solution objSolution;
+
+	for (size_t i = 0; i < vctInput.size(); ++i)
+	{
+		cout << endl << "Input: [ ";
+		printArray(vctInput[i]);
+		cout << "\b\b]" << endl;
+
+		iRet = objSolution.rob(vctInput[i]);
+		cout << "Output: [ ";
+		cout << iRet << "]" << endl;
+	}
+
+	return 0;
+}
+
+/*
+Input: [ 1, 2, 3, 1]
+Output: [ 4]
+
+Input: [ 2, 7, 9, 3, 1]
+Output: [ 12]
+*/
